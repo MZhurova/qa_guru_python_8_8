5df245fdadf4a3e5aa876a6cddf4e49494db6165
Mariya Zhurova <mzhurova4@mail.ru>
2023-10-21 20:11:19 +0700

    Готово

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -3,12 +3,17 @@
 """
 import pytest
 
-from homework.models import Product
+from homework.models import Product, Cart
 
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
+
+
+@pytest.fixture
+def cart():
+    return Cart()
 
 
 class TestProducts:
@@ -19,16 +24,20 @@ class TestProducts:
 
     def test_product_check_quantity(self, product):
         # TODO напишите проверки на метод check_quantity
-        pass
+        assert product.check_quantity(999)
+        assert product.check_quantity(1000)
+        assert not product.check_quantity(1001)
 
     def test_product_buy(self, product):
         # TODO напишите проверки на метод buy
-        pass
+        product.buy(999)
+        assert product.quantity == 1
 
     def test_product_buy_more_than_available(self, product):
         # TODO напишите проверки на метод buy,
         #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
-        pass
+        with pytest.raises(ValueError):
+            product.buy(1001)
 
 
 class TestCart:
@@ -38,3 +47,40 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+
+    def test_add(self, cart, product):
+        cart.add_product(product, 1)
+
+        assert cart.products[product] == 1
+
+        cart.add_product(product, 6)
+
+        assert cart.products[product] == 7
+
+    def test_delete(self, cart, product):
+        cart.add_product(product, 7)
+        cart.remove_product(product, 3)
+
+        assert cart.products[product] == 4
+
+        cart.remove_product(product, 4)
+
+        assert product not in cart.products
+
+    def test_clear(self, cart, product):
+        cart.add_product(product, 7)
+        cart.clear()
+
+        assert product not in cart.products
+
+    def test_buy(self, cart, product):
+        cart.add_product(product, 77)
+        cart.buy() == 90
+
+        assert product not in cart.products
+        assert product.quantity == 923
+
+    def test_buy_more(self, cart, product):
+        cart.add_product(product, 1001)
+        with pytest.raises(ValueError):
+            cart.buy()
